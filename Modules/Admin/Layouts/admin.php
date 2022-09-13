758ad3c8250cc0e448815c0d886d353a7390f976
Vipan Kumar <vipan.webdeveloper@gmail.com>
2022-09-13 22:47:05 +0530

    admin template init

--- Modules/Admin/Layouts/admin.php
+++ Modules/Admin/Layouts/admin.php
@@ -62,5 +62,26 @@
     </aside>
     <!-- /.control-sidebar -->
 </div>
+<!-- jQuery -->
+<script src="/plugins/jquery/jquery.min.js"></script>
+<!-- jQuery UI 1.11.4 -->
+<script src="/plugins/jquery-ui/jquery-ui.min.js"></script>
+<!-- Resolve conflict in jQuery UI tooltip with Bootstrap tooltip -->
+<script>
+    $.widget.bridge('uibutton', $.ui.button)
+</script>
+<script src="/plugins/bootstrap/js/bootstrap.bundle.min.js"></script>
+<script src="/plugins/chart.js/Chart.min.js"></script>
+<script src="/plugins/sparklines/sparkline.js"></script>
+<script src="/plugins/jqvmap/jquery.vmap.min.js"></script>
+<script src="/plugins/jqvmap/maps/jquery.vmap.usa.js"></script>
+<script src="/plugins/jquery-knob/jquery.knob.min.js"></script>
+<script src="/plugins/moment/moment.min.js"></script>
+<script src="/plugins/daterangepicker/daterangepicker.js"></script>
+<script src="/plugins/tempusdominus-bootstrap-4/js/tempusdominus-bootstrap-4.min.js"></script>
+<script src="/plugins/summernote/summernote-bs4.min.js"></script>
+<script src="/plugins/overlayScrollbars/js/jquery.overlayScrollbars.min.js"></script>
+<script src="/dist/js/adminlte.js"></script>
+
 </body>
 </html>
